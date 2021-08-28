79fdfa5459599120f35f86281154a78a0c643907
Olivia <88483682+OliviaCE@users.noreply.github.com>
2021-08-28 16:37:37 +0530

    A TOO SIMPLE IF CLASS ADDED

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 
 int main()
 {
-    firstcallfunc();
-    secondcallfunc();
+    int x = 1000;
+
+    const char name[7] = "Olivia";
+
+    std::cout << "My Name is " << name;
+
+    std::cout << "Simple OOP";
+
+    if (x == 1000) {
+        firstcallfunc();
+    } else {
+        secondcallfunc();
+    }
+
     return 0;
 }
 
